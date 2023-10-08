60013bfbbe4421dca7751cae9048f89066996c5e
danihdv <hdvgonz@gmail.com>
2023-10-08 14:22:04 -0500

    Mini-game completed

--- main.go
+++ main.go
@@ -7,26 +7,49 @@ import (
 
 func main() {
 
- play()
+	play()
 }
 
-func play (){
+func play() {
 	numAleatorio := rand.Intn(100)
-	fmt.Println(numAleatorio)
-	var numIngresado int;
-	var numIntentos int;
-	const MaxIntentos = 10;
+	var numIngresado int
+	var numIntentos int
+	const MaxIntentos = 10
 
-	for numIntentos < MaxIntentos{
-		numIntentos++;
-		fmt.Printf("Ingrese número: ");
-		fmt.Scan(&numIngresado);
+	for numIntentos < MaxIntentos {
+		numIntentos++
+		fmt.Printf("\nIngrese número: ")
+		fmt.Scan(&numIngresado)
 
-		if numIngresado == numAleatorio{
-			fmt.Println("¡Felicidades has Ganado!");
-			break;
-		}else{
-			fmt.Printf("¡Fallaste! sigue intentando, te quedan %d", MaxIntentos - numIntentos)
+		if numIngresado == numAleatorio {
+			fmt.Println("¡Felicidades has Ganado!, el numero sorpresa es: ", numAleatorio)
+			break
+		} else if numIngresado < numAleatorio {
+			fmt.Println("El número es mayor, ¡Súbele un poco! \n el número de intentos restantes es: ", MaxIntentos-numIntentos)
+		} else if numIngresado > numAleatorio {
+			fmt.Println("El número es menor, ¡Bájale un poco! \n el número de intentos restantes es: ", MaxIntentos-numIntentos)
 		}
 	}
+
+	fmt.Println("¡Has perdido!, Mejor suerte a la próxima, el numero sorpresa era: ", numAleatorio)
+	playAgain()
+}
+
+func playAgain() {
+	var choice string
+
+	fmt.Println("¿Desea jugar de nuevo? (s/n)")
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case "s":
+		play()
+	case "n":
+		fmt.Println("Es una pena ='(, esperamos que regreses pronto")
+	
+	default:
+		fmt.Println("Por favor, ingresa una opción válida, ¡Hasta la próxima!")
+
+	}
+
 }
